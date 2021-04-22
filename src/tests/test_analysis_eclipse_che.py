f8120896149088a87501b06e1cce8331a3616a02
nitishSr <nsrivast@redhat.com>
2021-04-22 18:18:45 +0530

    Modified test to call the create config func

--- src/tests/test_analysis_eclipse_che.py
+++ src/tests/test_analysis_eclipse_che.py
@@ -4,5 +4,6 @@ def test_run_analysis(setup_eclipse_che):
     and assert the story points
     """
     eclipse_che = setup_eclipse_che
+    eclipse_che.create_run_configuration()
     story_points = eclipse_che.run_analysis()
     assert int(story_points) == 35
