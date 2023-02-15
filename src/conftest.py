f120304047e817fdd09921d2fa08c795f8a443a7
midays <midays@redhat.com>
2023-02-15 19:02:58 +0200

    changed structure

--- src/conftest.py
+++ src/conftest.py
@@ -1,1 +1,12 @@
 pytest_plugins = ["src.infra.fixtures.ide_fixture"]
+
+
+# def pytest_generate_tests(metafunc):
+#     if "configuration_name" in metafunc.fixturenames:
+#         metafunc.parametrize("configuration_name", ["from parameters", "from parameters 1"])
+#
+#     if "target" in metafunc.fixturenames:
+#         metafunc.parametrize("target", ["eap7", "openjdk17"])
+#
+#     if "source" in metafunc.fixturenames:
+#         metafunc.parametrize("source", ["eap5", "openjdk11"])
