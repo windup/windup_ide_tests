d7e9f079bdfdbc4f745ec6225328d28a21e34f47
Nandini Chandra <nachandr@redhat.com>
2023-02-21 09:21:51 -0600

    Add openDJK17 target
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- src/tests/test_analysis_vscode.py
+++ src/tests/test_analysis_vscode.py
@@ -1,51 +1,35 @@
-def test_analysis_eap(setup_vscode):
+import pytest
+
+from src.lib.config import config_data
+
+
+@pytest.mark.parametrize('migration_path', ['eap7-genereic to eap7', 'thorntail to eapxp', 'springboot to quarkus',
+        'openjdk17'])
+def test_analysis(setup_vscode, migration_path):
     """
     Test to run an analysis on migration from weblogic to EAP 7 in VSCode IDE
     source : windup-rulesets/target/classes/eap7/weblogic/tests/data
     target : eap7
     """
-    vscode, config = setup_vscode
+    vscode = setup_vscode
     vscode.open_mta_perspective()
+    if migration_path == 'eap7-genereic to eap7':
+        source=config["project_paths"]["eap7_generic"],
+        target="eap7"
+    elif migration_path == 'thorntail to eapxp':
+        source=config["project_paths"]["eapxp_ruleset"],
+        target="eapxp"
+    elif migration_path == 'springboot to quarkus':
+        source=config["project_paths"]["quarkus_ruleset"],
+        target="quarkus"
+    elif migration_path == 'openjdk17':
+        source=config["project_paths"]["openjdk17"],
+        target="openjdk17"
     vscode.run_simple_analysis(
-        project=config["project_paths"]["eap7_generic"],
-        migration_target="eap7",
+        project=source,
+        migration_target=target,
     )
     assert vscode.is_analysis_complete()
     vscode.open_report_page()
     assert vscode.verify_story_points(target="eap7")
-
-
-def test_analysis_eapxp(setup_vscode):
-    """
-    Test to run an analysis on migration from thorntail to eapxp2 in VSCode IDE
-    source : windup-rulesets/target/classes/eapxp/thorntail/tests/data
-    target : eapxp
-    """
-    vscode, config = setup_vscode
-    vscode.open_mta_perspective()
-    vscode.run_simple_analysis(
-        project=config["project_paths"]["eapxp_ruleset"],
-        migration_target="eapxp",
-    )
-    assert vscode.is_analysis_complete()
-    vscode.open_report_page()
-    assert vscode.verify_story_points(target="eapxp")
-
-
-def test_analysis_quarkus(setup_vscode):
-    """
-    Test to run an analysis on migration from quarkus in VSCode IDE. Note that quarkus1 was removed
-    as source and target through https://issues.redhat.com/browse/WINDUP-3328 .
-
-    source : windup-rulesets/target/classes/quarkus/springboot/tests/data
-    target : quarkus
-    """
-    vscode, config = setup_vscode
-    vscode.open_mta_perspective()
-    vscode.run_simple_analysis(
-        project=config["project_paths"]["quarkus_ruleset"],
-        migration_target="quarkus",
-    )
-    assert vscode.is_analysis_complete()
-    vscode.open_report_page()
-    assert vscode.verify_story_points(target="quarkus")
+    
