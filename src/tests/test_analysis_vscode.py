639b3d04da5a26c55f0c44449830ba813aeb8657
Nandini Chandra <nachandr@redhat.com>
2023-02-27 13:02:35 -0600

    Update parametrization stuff for VSCode
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- src/tests/test_analysis_vscode.py
+++ src/tests/test_analysis_vscode.py
@@ -3,24 +3,14 @@ import pytest
 
 @pytest.mark.parametrize(
     "migration_path",
-    ["weblogic_to_eap7", "thorntail_to_eapxp2", "springboot_to_quarkus", "eap_to_azure-appservice"],
+    ["weblogic_to_eap7", "thorntail_to_eapxp", "springboot_to_quarkus", "eap_to_azure-appservice"],
 )
 def test_analysis(setup_vscode, migration_path):
     """Analysis tests for VScode using various migration paths"""
     vscode, config = setup_vscode
     vscode.open_mta_perspective()
-    if migration_path == "weblogic_to_eap7":
-        project = config["project_paths"]["eap7_generic"]
-        migration_target = "eap7"
-    elif migration_path == "thorntail_to_eapxp2":
-        project = config["project_paths"]["eapxp_ruleset"]
-        migration_target = "eapxp"
-    elif migration_path == "springboot_to_quarkus":
-        project = config["project_paths"]["quarkus_ruleset"]
-        migration_target = "quarkus"
-    else:
-        project = config["project_paths"]["azure_ruleset"]
-        migration_target = "azure-appservice"
+    project = config["project_path"][migration_path]["path"]
+    migration_target = config["project_path"][migration_path]["targets"]
     vscode.run_simple_analysis(project, migration_target)
     assert vscode.is_analysis_complete()
     vscode.open_report_page()
