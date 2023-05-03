3d740e95ea0ca74f460fdce68f4d3b9b1db75bf1
midays <midays@redhat.com>
2023-05-03 18:33:53 +0300

    remove print

--- src/tests/test_analysis_intellij.py
+++ src/tests/test_analysis_intellij.py
@@ -16,12 +16,10 @@ def test_analysis_intellij(configurations, setup_intellij, app_name, analysis_da
     intellij = setup_intellij
     application_data = analysis_data[app_name]
     migration_targets = application_data["targets"]
-    print(migration_targets)
     # Intellij freezes without this sleep
     time.sleep(3)
     intellij.run_simple_analysis(app_name)
     intellij.open_report_page(app_name)
 
     assert intellij.is_analysis_complete()
-    # remove for now, will enhance in next PR
-    # assert intellij.verify_story_points(target=migration_targets[0])
+    assert intellij.verify_story_points(target=migration_targets[0])
