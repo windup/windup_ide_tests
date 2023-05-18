6e9007df9853550caf90bd86070acbe421cfc255
Nandini Chandra <nachandr@redhat.com>
2023-05-18 13:06:54 -0500

    Add assertion for *.csv files
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- src/tests/test_advanced_options.py
+++ src/tests/test_advanced_options.py
@@ -10,10 +10,9 @@ DATA_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/data
 @pytest.mark.parametrize("ide", ["vscode"])
 def test_csv_report_vscode(configurations, setup_vscode, analysis_data, ide):
     """Analysis tests for VScode using various migration paths"""
-    pytest.set_trace()
-    config_object = configurations
-    output = config_object[0].options.output
     vscode = setup_vscode
+    conf_object = configurations
+    output_path = conf_object.configurations[0].options.output
     application_data = analysis_data["weblogic_to_eap7_export"]
     project = application_data["path"]
     migration_target = application_data["targets"]
@@ -22,6 +21,6 @@ def test_csv_report_vscode(configurations, setup_vscode, analysis_data, ide):
     vscode.run_simple_analysis(project, migration_target)
     assert vscode.is_analysis_complete()
 
-    assert_valid_csv(os.path.join(output, 'AllIssues.csv'))
-    assert_valid_csv(os.path.join(output, 'ApplicationFileTechnologies.csv'))
-    assert_valid_csv(os.path.join(output, 'jee_example_app_1_0_0_ear.csv'))
+    # Assert *.csv files are generated after analysis
+    assert_valid_csv_file(os.path.join(output_path, 'AllIssues.csv'))
+    assert_valid_csv_file(os.path.join(output_path, 'ApplicationFileTechnologies.csv'))
--- src/utils/general.py
+++ src/utils/general.py
@@ -1,3 +1,4 @@
+import csv
 import uuid
 
 
@@ -14,3 +15,27 @@ def read_file(filename):
     with open(filename, "r") as file:
         contents = file.read()
     return contents
+
+def assert_valid_csv_file(file_path, **kwargs):
+    """
+        Asserts that the given CSV file is valid.
+        A CSV file is considered valid if it exists and all of its rows have the same number of columns.
+
+        Args:
+            file_path (str): The file path of the CSV file to validate.
+            **kwargs: Optional keyword arguments.
+                delimiter (str): The delimiter used in the CSV file. Defaults to ','.
+
+        Raises:
+            AssertionError: If the CSV file does not exist or if its rows have different numbers of columns.
+
+        Returns:
+            None.
+    """
+    assert os.path.exists(csv_file_path) is True
+    delimiter = kwargs.get('delimiter', ',')
+
+    with open(file_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=delimiter)
+        columns_number = len(next(csv_reader))
+        assert all(len(row) == columns_number for row in csv_reader) is True
