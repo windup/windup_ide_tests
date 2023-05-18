c118c0100c3bca629f37b49ca3be3e257e028bff
Nandini Chandra <nachandr@redhat.com>
2023-05-18 18:56:51 -0500

    Add export csv test for intellij
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ src/tests/test_advanced_options_vscode.py
@@ -0,0 +1,23 @@
+import pytest
+from utils.general import assert_valid_csv_file
+
+
+@pytest.mark.parametrize("app_name", ["weblogic_to_eap7_export_csv"])
+@pytest.mark.parametrize("ide", ["vscode"])
+def test_csv_report_vscode(configurations, setup_vscode, app_name, analysis_data, ide):
+    """Analysis tests for VScode using various advanced options"""
+    vscode = setup_vscode
+    conf_object = configurations
+    output_path = conf_object.configurations[0].options.output
+    application_data = analysis_data[app_name]
+    project = application_data["path"]
+    migration_target = application_data["targets"]
+
+    vscode.open_mta_perspective()
+    vscode.run_simple_analysis(project, migration_target)
+    assert vscode.is_analysis_complete()
+
+    # Assert *.csv files are generated after analysis
+    assert_valid_csv_file(output_path, "AllIssues.csv")
+    assert_valid_csv_file(output_path, "ApplicationFileTechnologies.csv")
+    assert_valid_csv_file(output_path, "data.csv")
--- src/utils/general.py
+++ src/utils/general.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import uuid
 
 
@@ -16,15 +17,16 @@ def read_file(filename):
         contents = file.read()
     return contents
 
+
 def assert_valid_csv_file(file_path, **kwargs):
     """
-        Asserts that the given .csv file is valid.
-        A CSV file is considered valid if  all of its rows have the same number of columns.
+    Asserts that the given .csv file is valid.
+    A CSV file is considered valid if  all of its rows have the same number of columns.
     """
-    assert os.path.exists(csv_file_path) is True
-    delimiter = kwargs.get('delimiter', ',')
+    assert os.path.exists(file_path) is True
+    delimiter = kwargs.get("delimiter", ",")
 
-    with open(file_path, mode='r') as csv_file:
+    with open(file_path, mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=delimiter)
         columns_number = len(next(csv_reader))
         assert all(len(row) == columns_number for row in csv_reader) is True
