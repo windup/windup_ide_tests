fb6241bb989c9f5e221672cd9a8072aa61c359fb
Nandini Chandra <nachandr@redhat.com>
2023-05-26 15:21:34 -0500

    [RFR] Analysis with --skipReports option (#33)
    * Analysis with --skipReports option
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add test for IntelliJ
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Update test parameter
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- src/tests/test_advanced_options_vscode.py
+++ src/tests/test_advanced_options_vscode.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from src.utils.general import assert_valid_csv_file
@@ -6,7 +8,7 @@ from src.utils.general import assert_valid_csv_file
 @pytest.mark.parametrize("app_name", ["thorntail_to_eapxp"])
 @pytest.mark.parametrize("ide", ["vscode"])
 def test_csv_report_vscode(configurations, setup_vscode, app_name, analysis_data, ide):
-    """Analysis tests for VScode using various advanced options"""
+    """Analysis test for VScode using --exportCSV option"""
     vscode = setup_vscode
     conf_object = configurations
     output_path = conf_object.configurations[0].options.output
@@ -22,3 +24,23 @@ def test_csv_report_vscode(configurations, setup_vscode, app_name, analysis_data
     assert_valid_csv_file(output_path, "AllIssues.csv")
     assert_valid_csv_file(output_path, "ApplicationFileTechnologies.csv")
     assert_valid_csv_file(output_path, "data.csv")
+
+
+@pytest.mark.parametrize("app_name", ["weblogic_to_eap7_skip_reports"])
+@pytest.mark.parametrize("ide", ["vscode"])
+def test_skip_reports_vscode(configurations, setup_vscode, app_name, analysis_data, ide):
+    """Analysis test for VScode using --skipReports option"""
+    vscode = setup_vscode
+    conf_object = configurations
+    output_path = conf_object.configurations[0].options.output
+    configuration_data = analysis_data[app_name]
+    project = configuration_data["paths"]
+    migration_target = configuration_data["targets"]
+
+    vscode.open_mta_perspective()
+    vscode.run_simple_analysis(project, migration_target)
+    assert vscode.is_analysis_complete()
+
+    # Assert index.html file is not generated after analysis with skipReports option
+    file_path = os.path.join(output_path, "index.html")
+    assert os.path.exists(file_path) is False
