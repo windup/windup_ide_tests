543c8e59f6f18cbcd41911cc7fd82d243c6a81b5
midays <92722851+midays@users.noreply.github.com>
2023-07-10 14:05:13 +0300

    [RFR] added TC for cancelling analysis in intellij (#36)
    * added TC for cancelling analysis in intellij
    
    * pre-commit -a

---
+++ src/tests/test_cancel_analysis_intellij.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+import pytest
+
+DATA_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/data/"
+
+
+@pytest.mark.parametrize("app_name", ["weblogic_to_eap7"])
+@pytest.mark.parametrize("ide", ["intellij"])
+def test_cancel_analysis_intellij(configurations, setup_intellij, app_name, analysis_data, ide):
+    """
+    Tests the behaviour of canceling an analysis
+    """
+    intellij = setup_intellij
+
+    # Intellij freezes without this sleep
+    time.sleep(3)
+    intellij.run_simple_analysis(app_name, wait_for_analysis_finish=False)
+    intellij.cancel_analysis()
+    intellij.wait_find_element(locator_type="image", locator="analysis_cancelled.png")
