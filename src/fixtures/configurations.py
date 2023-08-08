fe09d665369705559fad8f390d1ab8e72a420904
midays <midays@redhat.com>
2023-08-08 13:20:05 +0300

    Isolcated the creating of configuration in a erparated function so it can be called without using a fixture

--- src/fixtures/configurations.py
+++ src/fixtures/configurations.py
@@ -3,9 +3,7 @@ import os
 
 import pytest
 
-from src.models.configuration.configuration import Configuration
 from src.models.configuration.configurations_object import ConfigurationsObject
-from src.models.configuration.options import Options
 from src.models.IDE.Intellij import Intellij
 from src.models.IDE.VisualStudioCode import VisualStudioCode
 from src.utils.general import delete_directory
@@ -18,48 +16,29 @@ DATA_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/data
 
 @pytest.fixture(scope="function")
 def configurations(
-    config,
-    intellij_config,
-    vscode_config,
-    app_name,
-    analysis_data,
-    ide,
-    setup_intellij,
-    setup_vscode,
+        config,
+        intellij_config,
+        vscode_config,
+        app_name,
+        analysis_data,
+        ide,
+        setup_intellij,
+        setup_vscode,
 ):
-
     # region construct configuration object and fill it from the data json
 
     uuid = generate_uuid()
+
     application = setup_intellij if ide == "intellij1" else setup_vscode
     application_config = intellij_config if ide == "intellij" else vscode_config
     html_file_location = f"{application_config['plugin_cache_path']}/{uuid}/index.html"
+    model_json_path = f"{application_config['plugin_cache_path']}/model.json"
 
     configurations_object = ConfigurationsObject()
-
-    application_data = analysis_data[app_name]
-    model_json_path = f"{application_config['plugin_cache_path']}/model.json"
-    project_path = config["project_path"]
-
-    inputs = [os.path.join(project_path, path) for path in application_data["paths"]]
-
-    # Build data for analysis configuration
-    options = Options.from_dict(application_data["options"])
-    options.input = inputs
-    options.cli = config["windup_cli_path"]
-    options.source_mode = True
-    options.output = f"{application_config['plugin_cache_path']}/{uuid}"
-
-    configuration = Configuration(name=app_name, id=uuid, options=options)
-
-    configurations_object.configurations.append(configuration)
-    application.configurations = configurations_object.configurations
+    configuration = configurations_object.create(analysis_data, app_name, application_config, config, uuid)
+    application.configurations.append(configuration)
 
     # endregion
-
-    # convert the object to json and write to the model.json file
-    final_configuration_json = json.dumps(configurations_object.to_dict())
-    write_data_to_file(model_json_path, final_configuration_json)
 
     yield configurations_object, html_file_location
 
