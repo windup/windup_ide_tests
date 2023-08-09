029117efb49632afcd81e1f863f887e982900e05
midays <midays@redhat.com>
2023-08-09 12:03:51 +0300

    added tc for testing the input path when it's empty/ invalid

--- src/tests/test_input_validation_intellij.py
+++ src/tests/test_input_validation_intellij.py
@@ -1,40 +1,51 @@
-import os
 import time
 
 import pytest
 
-from src.models.configuration.configuration import Configuration
+from src.utils.general import generate_uuid
+from src.utils.ocr import find_all_sentence_occurrences
 
-DATA_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/data/"
 
-
-CONFIGURATION_JSON =   {
-    "paths": [
-      ""
-    ],
-    "options": {
-      "targets": [
-        "eap8"
-      ]
-    },
-    "story_points": [
-      117
-    ]
-  },
-
-@pytest.mark.parametrize("ide", ["intellij"])
-def test_input_validation(setup_intellij, app_name, ide):
+def test_empty_input_validation(setup_intellij):
     """
     Analysis tests for intellij using various migration paths
     """
 
-    config = Configuration.from_dict(CONFIGURATION_JSON)
     intellij = setup_intellij
-
-    intellij.refresh_configuration()
-
+    intellij.delete_all_configurations()
+    intellij.create_configuration_in_ui()
+    intellij.run_simple_analysis("configuration0")
 
     # Intellij freezes without this sleep
     time.sleep(3)
+
+    assert len(find_all_sentence_occurrences("Path to input required")) > 0
+
+
+@pytest.mark.parametrize("app_name", ["weblogic_to_eap7"])
+@pytest.mark.parametrize("ide", ["intellij"])
+def test_invalid_input_validation(
+    setup_intellij,
+    app_name,
+    intellij_config,
+    config,
+    analysis_data,
+    ide,
+):
+    """
+    Analysis tests for intellij using various migration paths
+    """
+
+    intellij = setup_intellij
+    intellij.delete_all_configurations()
+
+    analysis_data[app_name]["paths"][0] = "/invalid/input/path"
+    uuid = generate_uuid()
+
+    intellij.create_configuration_in_file(analysis_data, app_name, intellij_config, config, uuid)
     intellij.run_simple_analysis(app_name)
-    intellij.open_report_page(app_name)
+
+    # Intellij freezes without this sleep
+    time.sleep(3)
+
+    assert len(find_all_sentence_occurrences("Input location does not exist")) > 0
