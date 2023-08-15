1d5c84001c1e678dce6c4ac96dd43af16b7b9b69
midays <92722851+midays@users.noreply.github.com>
2023-08-15 16:45:52 +0300

    [RFR] intellij CLI validation (#37)
    * Created OCR function to extract a string from a screenshot
    
    * test empty CLI path
    
    * test empty CLI path
    
    * remove unused params
    
    * remove unused params
    
    * removed OCR for a separate branch
    
    * added tc for testing the cli path when it's empty/ invalid

---
+++ src/tests/test_cli_validation_intellij.py
@@ -0,0 +1,51 @@
+import time
+
+import pytest
+
+from src.utils.general import generate_uuid
+from src.utils.ocr import find_all_sentence_occurrences
+
+
+def test_empty_cli_path_intellij(setup_intellij):
+    """
+    Tests the validation of empty cli path
+    """
+    intellij = setup_intellij
+
+    # Intellij freezes without this sleep
+    time.sleep(3)
+
+    intellij.delete_all_configurations()
+    intellij.create_configuration_in_ui()
+    intellij.run_simple_analysis("configuration0")
+
+    assert len(find_all_sentence_occurrences("")) > 0
+
+
+@pytest.mark.parametrize("app_name", ["weblogic_to_eap7"])
+def test_invalid_cli_path_intellij(
+    setup_intellij,
+    intellij_config,
+    config,
+    app_name,
+    analysis_data,
+):
+    """
+    Tests the validation of invalid cli path
+    """
+    intellij = setup_intellij
+
+    # Intellij freezes without this sleep
+    time.sleep(3)
+
+    intellij.delete_all_configurations()
+    config["windup_cli_path"] = "path/dont/exist"
+    intellij.create_configuration_in_file(
+        analysis_data,
+        app_name,
+        intellij_config,
+        config,
+        uuid=generate_uuid(),
+    )
+
+    assert len(find_all_sentence_occurrences("Path to CLI executable does not exist")) > 0
