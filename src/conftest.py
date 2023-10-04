b48ca124af3bffbfce90522109ff897da5205c0e
midays <midays@redhat.com>
2023-10-04 14:03:46 +0300

    pre-commit

--- src/conftest.py
+++ src/conftest.py
@@ -1,4 +1,6 @@
-pytest_plugins = ["src.fixtures.ide",
-                  "src.fixtures.configurations",
-                  "src.fixtures.video_record",
-                  "src.fixtures.pytest_options"]
+pytest_plugins = [
+    "src.fixtures.ide",
+    "src.fixtures.configurations",
+    "src.fixtures.video_record",
+    "src.fixtures.pytest_options",
+]
