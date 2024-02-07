4582111518ef737596cb090bb0fc0b4f922da5e3
midays <midays@redhat.com>
2024-02-07 11:11:26 +0200

    remove mtr, and mta options support since it's only mta now

--- src/fixtures/pytest_options.py
+++ src/fixtures/pytest_options.py
@@ -3,16 +3,6 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--mtr",
-        action="store_true",
-        help="This is when running a test case on MTR (Migration Toolkit for Runtime) plugin.",
-    )
-    parser.addoption(
-        "--mta",
-        action="store_true",
-        help="This is when running a test case on MTA (Migration Toolkit for Application) plugin.",
-    )
-    parser.addoption(
         "--record-video",
         action="store_true",
         help="This options enables the record video fixture to record the test run.",
@@ -20,6 +10,4 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    pytest.mtr = config.getoption("--mtr")
-    pytest.mta = config.getoption("--mta")
     pytest.record_video = config.getoption("--record-video")
