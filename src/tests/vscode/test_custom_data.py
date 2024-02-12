543e0ac1eedc4cbda501d71ae88f30f67c1f62c0
midays <92722851+midays@users.noreply.github.com>
2024-02-12 17:57:30 +0200

    MTA 486 a custom target added in the UI should also be picked (#89)
    * MTA-486

--- src/tests/vscode/test_custom_data.py
+++ src/tests/vscode/test_custom_data.py
@@ -6,6 +6,12 @@ CUSTOM_SRC_JSON = [
     },
 ]
 
+CUSTOM_TGT_JSON = [
+    {
+        "custom targets": {"options": {"target": ["custom_target", "custom_target_1"]}},
+    },
+]
+
 
 @pytest.mark.parametrize("app_name", ["custom sources"])
 @pytest.mark.parametrize("analysis_data", CUSTOM_SRC_JSON)
@@ -28,3 +34,26 @@ def test_custom_source_vscode(setup_vscode, configurations, app_name, analysis_d
     command_source = command_map["source"]
 
     assert set(command_source) == set(sources_list), f"Error: the following sources were not picked by the IDE: {','.join([src for src in sources_list if src not in command_source])}"
+
+
+@pytest.mark.parametrize("app_name", ["custom targets"])
+@pytest.mark.parametrize("analysis_data", CUSTOM_TGT_JSON)
+@pytest.mark.parametrize("ide", ["vscode"])
+@pytest.mark.vscode
+def test_custom_target_vscode(setup_vscode, configurations, app_name, analysis_data, ide):
+    # Automates Polarion MTA-486
+
+    vscode = setup_vscode
+
+    configuration_object, _, _ = configurations
+
+    targets_list = configuration_object.configurations[0].options.target
+
+    vscode.set_focus()
+    vscode.run_simple_analysis(app_name)
+    vscode.cancel_analysis()
+
+    command_map = vscode.fetch_executed_cli_command_map()
+    command_target = command_map["target"]
+
+    assert set(command_target) == set(targets_list), f"Error: the following targets were not picked by the IDE: {','.join([tgt for tgt in targets_list if tgt not in command_target])}"
