aa53da0959c9768b4cd882af41c12a6531522f3d
midays <midays@redhat.com>
2024-02-13 13:29:36 +0200

    removed story points verification after analysis complete

--- src/tests/vscode/test_analysis.py
+++ src/tests/vscode/test_analysis.py
@@ -4,27 +4,18 @@ import os
 import pytest
 
 DATA_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/data/"
+MIN = 60
 
 
 @pytest.mark.parametrize("app_name", json.load(open(DATA_DIR + "analysis.json")))
 @pytest.mark.parametrize("ide", ["vscode"])
 @pytest.mark.vscode
 def test_analysis_vscode(setup_vscode, configurations, app_name, ide):
-    # def test_analysis_vscode(setup_vscode, configurations, app_name, analysis_data, ide):
     """Analysis tests for VScode using various migration paths"""
+    # Automates polarion MTA-475, MTA-476, MTA-477, MTA-478, MTA-479, MTA-480
     vscode = setup_vscode
-    # application_data = analysis_data[app_name]
-
-    _, html_file_location, _ = configurations
-    # expected_story_points = application_data["story_points"]
-
     vscode.set_focus()
     vscode.open_mta_perspective()
     vscode.run_simple_analysis(app_name)
-    status, message = vscode.is_analysis_complete(timeout=(5 * 60))
+    status, message = vscode.is_analysis_complete(timeout=(5 * MIN))
     assert status, message
-
-    # vscode.verify_story_points(
-    #     html_file_location=html_file_location,
-    #     expected_story_points=expected_story_points,
-    # )
