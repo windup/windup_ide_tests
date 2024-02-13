eb130f4288511874a1f98e310dada6f77c69db61
midays <92722851+midays@users.noreply.github.com>
2024-02-13 18:33:52 +0200

    MTA-481 (#98)

--- src/tests/vscode/test_advanced_options.py
+++ src/tests/vscode/test_advanced_options.py
@@ -1,42 +1,31 @@
-import os
-
 import pytest
 
-from src.utils.general import assert_valid_csv_file
+CONF_JSON = [
+    {
+        "overwrite analysis": {"options": {"target": ["eap8"]}},
+    },
+]
 
 
-@pytest.mark.parametrize("app_name", ["thorntail_to_eapxp"])
+@pytest.mark.parametrize("app_name", ["overwrite analysis"])
+@pytest.mark.parametrize("analysis_data", CONF_JSON)
 @pytest.mark.parametrize("ide", ["vscode"])
 @pytest.mark.vscode
-def test_csv_report_vscode(configurations, setup_vscode, app_name, analysis_data, ide):
-    """Analysis test for VScode using --exportCSV option"""
+def test_overwrite_option(setup_vscode, vscode_config, configurations, app_name, analysis_data, ide):
+    # Automates polarion MTA-481
     vscode = setup_vscode
-    conf_object = configurations
-    output_path = conf_object.configurations[0].options.output
+    vscode.set_focus()
 
-    vscode.open_mta_perspective()
-    vscode.run_simple_analysis()
+    vscode.run_simple_analysis(app_name)
     assert vscode.is_analysis_complete()
 
-    # Assert *.csv files are generated after analysis
-    assert_valid_csv_file(output_path, "AllIssues.csv")
-    assert_valid_csv_file(output_path, "ApplicationFileTechnologies.csv")
-    assert_valid_csv_file(output_path, "data.csv")
+    configurations_object, _, _ = configurations
+    running_configuration = configurations_object.configurations[0]
+    running_configuration.options.overwrite = True
 
+    model_json_path = f"{vscode_config['plugin_cache_path']}/model.json"
+    vscode.update_configuration(model_json_path, running_configuration)
 
-@pytest.mark.parametrize("app_name", ["weblogic_to_eap7_skip_reports"])
-@pytest.mark.parametrize("ide", ["vscode"])
-@pytest.mark.vscode
-def test_skip_reports_vscode(configurations, setup_vscode, app_name, analysis_data, ide):
-    """Analysis test for VScode using --skipReports option"""
-    vscode = setup_vscode
-    conf_object = configurations
-    output_path = conf_object.configurations[0].options.output
-
-    vscode.open_mta_perspective()
-    vscode.run_simple_analysis()
+    vscode.clear_terminal_output_panel()
+    vscode.run_simple_analysis(app_name)
     assert vscode.is_analysis_complete()
-
-    # Assert index.html file is not generated after analysis with skipReports option
-    file_path = os.path.join(output_path, "index.html")
-    assert os.path.exists(file_path) is False
