05629067f207db4aebe8679cc16d949824ca3b9b
midays <midays@redhat.com>
2024-02-14 14:26:07 +0200

    added summery object

--- src/models/configuration/configuration.py
+++ src/models/configuration/configuration.py
@@ -1,7 +1,8 @@
 from typing import Any
 
 from src.models.configuration.options import Options
-from src.utils.data_control import from_str
+from src.models.configuration.summery import Summary
+from src.utils.data_control import from_str, from_none, from_union
 from src.utils.data_control import to_class
 
 
@@ -9,11 +10,13 @@ class Configuration:
     name: str
     options: Options
     id: str
+    summary: Summary
 
-    def __init__(self, name: str = None, options: Options = Options(), id: str = None) -> None:
+    def __init__(self, name: str = None, options: Options = Options(), id: str = None, summary=Summary()) -> None:
         self.name = name
         self.options = options
         self.id = id
+        self.summary = summary
 
     @staticmethod
     def from_dict(obj: Any) -> "Configuration":
@@ -21,15 +24,21 @@ class Configuration:
         name = from_str(obj.get("name"))
         options = Options.from_dict(obj.get("options"))
         id = obj.get("id")
-        return Configuration(name, options, id)
+        summary = from_union([Summary.from_dict, from_none], obj.get("summary"))
+        return Configuration(name, options, id, summary)
 
     def to_dict(self) -> dict:
         result: dict = {}
         result["name"] = from_str(self.name)
         result["options"] = to_class(Options, self.options)
         result["id"] = str(self.id)
+        if self.summary is not None:
+            result["summary"] = from_union([lambda x: to_class(Summary, x), from_none], self.summary)
         return result
 
     def add_options(self, options: []):
         for option, value in options:
             self.options.__setattr__(option, value)
+
+    def update_summery(self, summery: Summary):
+        self.summary = summery
---
+++ src/models/configuration/summery.py
@@ -0,0 +1,70 @@
+from typing import Optional, Any, Dict
+
+from src.utils.data_control import from_union, from_bool, from_none, from_str, from_int
+
+
+class Summary:
+    skipped_reports: Optional[bool]
+    output_location: Optional[str]
+    executed_timestamp: Optional[str]
+    executable: Optional[str]
+    executed_timestamp_raw: Optional[str]
+    active: Optional[bool]
+    quickfixes: Optional[Dict[str, Any]]
+    hint_count: Optional[int]
+    classification_count: Optional[int]
+
+    def __init__(self, skipped_reports: Optional[bool] = False, output_location: Optional[str] = "",
+                 executed_timestamp: Optional[str] = "", executable: Optional[str] = "",
+                 executed_timestamp_raw: Optional[str] = "",
+                 active: Optional[bool] = False, quickfixes=None,
+                 hint_count: Optional[int] = 0,
+                 classification_count: Optional[int] = 0) -> None:
+        if quickfixes is None:
+            quickfixes = {}
+        self.skipped_reports = skipped_reports
+        self.output_location = output_location
+        self.executed_timestamp = executed_timestamp
+        self.executable = executable
+        self.executed_timestamp_raw = executed_timestamp_raw
+        self.active = active
+        self.quickfixes = quickfixes
+        self.hint_count = hint_count
+        self.classification_count = classification_count
+
+    @staticmethod
+    def from_dict(obj: Any, from_int=None) -> 'Summary':
+        assert isinstance(obj, dict)
+        skipped_reports = from_union([from_bool, from_none], obj.get("skippedReports"))
+        output_location = from_union([from_str, from_none], obj.get("outputLocation"))
+        executed_timestamp = from_union([from_str, from_none], obj.get("executedTimestamp"))
+        executable = from_union([from_str, from_none], obj.get("executable"))
+        executed_timestamp_raw = from_union([from_str, from_none], obj.get("executedTimestampRaw"))
+        active = from_union([from_bool, from_none], obj.get("active"))
+        quickfixes = from_union([lambda x: x, from_none], obj.get("quickfixes"))
+        hint_count = from_union([from_int, from_none], obj.get("hintCount"))
+        classification_count = from_union([from_int, from_none], obj.get("classificationCount"))
+        return Summary(skipped_reports, output_location, executed_timestamp, executable, executed_timestamp_raw, active,
+                       quickfixes, hint_count, classification_count)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        if self.skipped_reports is not None:
+            result["skippedReports"] = from_union([from_bool, from_none], self.skipped_reports)
+        if self.output_location is not None:
+            result["outputLocation"] = from_union([from_str, from_none], self.output_location)
+        if self.executed_timestamp is not None:
+            result["executedTimestamp"] = from_union([from_str, from_none], self.executed_timestamp)
+        if self.executable is not None:
+            result["executable"] = from_union([from_str, from_none], self.executable)
+        if self.executed_timestamp_raw is not None:
+            result["executedTimestampRaw"] = from_union([from_str, from_none], self.executed_timestamp_raw)
+        if self.active is not None:
+            result["active"] = from_union([from_bool, from_none], self.active)
+        if self.quickfixes is not None:
+            result["quickfixes"] = from_union([lambda x: x, from_none], self.quickfixes)
+        if self.hint_count is not None:
+            result["hintCount"] = from_union([from_int, from_none], self.hint_count)
+        if self.classification_count is not None:
+            result["classificationCount"] = from_union([from_int, from_none], self.classification_count)
+        return result
