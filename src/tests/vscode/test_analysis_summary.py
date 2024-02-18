6c6849cfc3c087a35e0b8b30e1e487c397599ff0
midays <92722851+midays@users.noreply.github.com>
2024-02-18 13:33:01 +0200

    Added TC for MTA-513 (#114)

--- src/tests/vscode/test_analysis_summary.py
+++ src/tests/vscode/test_analysis_summary.py
@@ -1,12 +1,7 @@
-import os
-
 import pytest
 
 from src.models.configuration.configuration import Configuration
 from src.models.configuration.summery import Summary
-
-DATA_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/data/"
-MIN = 60
 
 
 @pytest.mark.parametrize("app_name", ["classifications check"])
@@ -26,10 +21,36 @@ def test_no_generated_classifications(setup_vscode, configurations, app_name, an
     vscode.set_focus()
     vscode.open_mta_perspective()
     vscode.run_simple_analysis(app_name)
-    status, message = vscode.is_analysis_complete(timeout=(5 * MIN))
+    status, message = vscode.is_analysis_complete()
     assert status, message
 
     configuration: Configuration = vscode.configurations_object.get_configuration(app_name)
     analysis_summery: Summary = configuration.summary
 
     assert analysis_summery.classification_count == 0, f"{str(analysis_summery.classification_count)} classifications were generated"
+
+
+@pytest.mark.parametrize("app_name", ["quickfixes check"])
+@pytest.mark.parametrize(
+    "analysis_data",
+    [
+        {
+            "quickfixes check": {"options": {"target": ["eap8"]}},
+        },
+    ],
+)
+@pytest.mark.parametrize("ide", ["vscode"])
+@pytest.mark.vscode
+def test_no_quickfixes_generated(setup_vscode, configurations, app_name, analysis_data, ide):
+    # Automates polarion MTA-513
+    vscode = setup_vscode
+    vscode.set_focus()
+    vscode.open_mta_perspective()
+    vscode.run_simple_analysis(app_name)
+    status, message = vscode.is_analysis_complete()
+    assert status, message
+
+    configuration: Configuration = vscode.configurations_object.get_configuration(app_name)
+    analysis_summery: Summary = configuration.summary
+
+    assert analysis_summery.quickfixes == {}, f"quickfixes were generated: {analysis_summery.quickfixes}"
