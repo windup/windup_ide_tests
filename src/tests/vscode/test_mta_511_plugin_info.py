74522de1ffd4fc9e651ccfd48727d81d481ae942
midays <92722851+midays@users.noreply.github.com>
2024-05-30 10:03:18 -0400

    Updated MTA-511 TC (#147)
    * Updated MTA 511 test case, and changed the expected plugin info, and added a new config variable i.e. "extension_path" in vscode_config.json
    
    * ignore vscode_plugin_info.txt when checking end of file

--- src/tests/vscode/test_mta_511_plugin_info.py
+++ src/tests/vscode/test_mta_511_plugin_info.py
@@ -2,19 +2,22 @@ import os
 
 import pytest
 
+from src.utils.general import find_mta_directory
 from src.utils.general import read_file
 
 DATA_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/data/"
 
 
 @pytest.mark.vscode
-def test_mta_511_plugin_info(setup_vscode):
+def test_mta_511_plugin_info(setup_vscode, vscode_config):
     # Automates Polarion MTA-511
-
     vscode = setup_vscode
     vscode.set_focus()
-    fetched_vscode_plugin_info = vscode.get_plugin_text()
-    expected_vscode_plugin_info = read_file(DATA_DIR + "vscode_plugin_info.txt").split("\n")
+
+    extensions_path = vscode_config["extensions_path"]
+    readme_path = os.path.join(find_mta_directory(extensions_path), "README.md")
+    fetched_vscode_plugin_info = vscode.get_plugin_details_text_from_file(readme_path)
+    expected_vscode_plugin_info = read_file(DATA_DIR + "vscode_plugin_info.txt")
     vscode.close_active_tab()
 
-    assert set(expected_vscode_plugin_info) == set(fetched_vscode_plugin_info)
+    assert expected_vscode_plugin_info == fetched_vscode_plugin_info
